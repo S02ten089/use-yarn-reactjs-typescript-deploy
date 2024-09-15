f9fdd0092e13dcd6aed3af07178e036635250f75
TienVu <moameicute25702@gmail.com>
2024-09-15 10:38:46 +0700

    tranport

---
+++ src/compoment/auth/ui/thirdweb/login.tsx
@@ -0,0 +1,42 @@
+// import { createThirdwebClient } from "thirdweb";
+// import { ConnectButton } from "thirdweb/react";
+
+// import { inAppWallet } from "thirdweb/wallets";
+
+// const client = createThirdwebClient({
+//   clientId: "....",
+// });
+
+// const wallets = [
+//   inAppWallet({
+//     auth: {
+//       options: [
+//         "google",
+//         "email",
+//         "phone",
+//         "facebook",
+//         "passkey",
+//         "x",
+//         "telegram",
+//         "discord",
+//       ],
+//     },
+//   }),
+// ];
+
+// function Example() {
+//   return (
+//     <ConnectButton
+//       client={client}
+//       wallets={wallets}
+//       connectModal={{
+//         size: "compact",
+//         title: "Đăng Nhập",
+//         titleIcon: "null-logo",
+//         termsOfServiceUrl: "null-dv",
+//         privacyPolicyUrl: "null-bm",
+//       }}
+//       locale={"vi_VN"}
+//     />
+//   );
+// }
