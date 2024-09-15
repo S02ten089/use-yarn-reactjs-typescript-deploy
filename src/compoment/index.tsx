d2617aab2de1f5ab8ab897366669001c9770b2f4
TienVu <moameicute25702@gmail.com>
2024-09-16 04:01:33 +0700

    edit public -- add xlsx

--- src/compoment/index.tsx
+++ src/compoment/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Chat from './chat/chat';
-import QR from './ux/emylays/createQR/qr';
-import ImageToBase64 from './ux/emylays/base64/base64';
-import Base64ToImage from './ux/emylays/base64/base64toimg';
+import QR from './ux/createQR/qr';
+import ImageToBase64 from './ux/base64/base64';
+import Base64ToImage from './ux/base64/base64toimg';
+import ExcelReader from './ux/xlsx';
 
 import ImageComponent from '../showimg/test/img';
 // import header
@@ -16,8 +17,12 @@ const App: React.FC = () => {
       <h1>Chat Application</h1>
       {/* <Chat /> */}
       <QR/>
+      <br />
       <ImageToBase64 />
+      <br />
       <Base64ToImage/>
+      <br />
+      <ExcelReader/>
       <div>
         <ImageComponent/>
       </div>
---
+++ src/compoment/ux/xlsx/index.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import * as XLSX from 'xlsx';
+
+const ExcelReader: React.FC = () => {
+  const [data, setData] = useState<any[]>([]);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const binaryStr = e.target?.result;
+        const workbook = XLSX.read(binaryStr, { type: 'binary' });
+
+        // Đọc dữ liệu từ sheet đầu tiên
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet);
+        setData(jsonData); // Lưu dữ liệu đọc được vào state
+      };
+
+      reader.readAsBinaryString(file);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Đọc file Excel (.xlsx)</h1>
+      <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
+      
+      {data.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, i) => (
+                  <td key={i}>
+                    {String(value)}
+                    {/* {typeof value === 'string' || typeof value === 'number' ? value : JSON.stringify(value)} */}
+                  </td> // Hiển thị giá trị
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default ExcelReader;
