52eab1e6d241f53f74b69bf0da8c57185be5f9fb
TienVu <moameicute25702@gmail.com>
2024-10-14 10:43:31 +0700

    nextjs build cache

---
+++ .next/server/chunks/42.js
@@ -0,0 +1,1 @@
+"use strict";exports.id=42,exports.ids=[42],exports.modules={1042:(i,s,e)=>{e.r(s),e.d(s,{default:()=>d});var r=e(2051);e(6269);var t=e(422);let d=()=>(0,r.jsxs)(t.xu,{children:[r.jsx("h1",{children:"Li\xean Hệ"}),r.jsx("p",{children:"Li\xean hệ với ch\xfang t\xf4i qua email hoặc số điện thoại."})]})}};
