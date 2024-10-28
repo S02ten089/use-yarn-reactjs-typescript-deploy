ba8e91d3d1894064fbe1c405d2e67cb60b12e3de
TienVu <moameicute25702@gmail.com>
2024-10-28 08:04:22 +0700

    edit footer 2-%

--- src/compoment/Contact/eng/index.tsx
+++ src/compoment/Contact/eng/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Contact from './Contact';
+import { Box } from '@chakra-ui/react';
+import UiUseCka from '../../ui/header/UiUseFramworkCka/App';
+import Footer from '../../ui/fooder/footer';
 
 const AppContactEng: React.FC = () => {
   return (
-    <div>
+    <Box
+    w='100%'
+    // h='200px'
+    bgGradient={[
+      'linear(to-tr, teal.300, yellow.400)',
+      'linear(to-t, blue.200, teal.500)',
+      'linear(to-b, orange.100, purple.300)',
+    ]}>
+      <UiUseCka/>
       <Contact/>
-    </div>
+      <Footer/>
+    </Box>
   );
 };
 
