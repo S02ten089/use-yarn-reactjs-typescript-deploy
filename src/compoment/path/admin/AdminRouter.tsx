88b3ee2ceff7430a2a7bcffe315cab3a0839a73c
TienVu <moameicute25702@gmail.com>
2024-12-09 20:49:24 +0700

    add TX

--- src/compoment/path/admin/AdminRouter.tsx
+++ src/compoment/path/admin/AdminRouter.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./dashboard/Dashboard";
 import ManageUsers from "./dashboard/manage/ManageUsers";
 import ManagePosts from "./dashboard/manage/ManagePosts";
+import ManageTX from "./dashboard/manage/black/ManageTX";
 
 const AdminRouter: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const AdminRouter: React.FC = () => {
       <Route path="dashboard" element={<Dashboard />} />
       <Route path="manage-users" element={<ManageUsers />} />
       <Route path="manage-posts" element={<ManagePosts />} />
+      <Route path="manage-TX" element={<ManageTX />} />
       {/* Redirect tất cả các đường dẫn admin không hợp lệ về dashboard */}
       <Route path="*" element={<Navigate to="dashboard" />} />
     </Routes>
